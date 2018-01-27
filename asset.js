3dcb7aaec974a9047e5c705fce958ad617309676
willieLOH <willie.loh@live.com>
2018-01-27 14:39:26 +0800

    moving tree

--- asset.js
+++ asset.js
@@ -10,9 +10,14 @@ window.onload = () => {
 
   ground = new Image();
   ground.src = "images/ground.png";
-  ground.onload = () => {
-    context.drawImage(ground, 0, 200);
-  }
+  ground.onload = () => { context.drawImage(ground, 0, 200); }
+
+  tree_img = new Image();
+  tree_img.src = "images/tree.png";
+  tree_img.onload = () => { context.drawImage(tree_img, 500, 140); }
+
+  planTree = (w = 0) => { return { img: tree_img, x: 450 + w, y: 140 }; }
+  moveTree = (tree) => { return (tree.x -= 10); }
 
   var lastDraw = 0;
 
@@ -21,18 +26,23 @@ window.onload = () => {
     if(shouldDraw){
       lastDraw = timestamp;
       console.log("draw");
-      context.clearRect(0, 0, 70, 200);
-      context.drawImage(dino, 0, 130);
+      context.clearRect(0, 0, 480, 200);
+      context.drawImage(dino_img, 0, 130);
+
+      if(typeof(tree) === "undefined"){ tree = planTree(); }
+      if(typeof(tree) && tree.x < -100){ tree = planTree(); }
+      if(typeof(tree)) { moveTree(tree); }
+      context.drawImage(tree.img, tree.x, tree.y);
     }
 
-      window.addEventListener("keypress", (event) => {
-        switch(event.key){
-          case ' ':
-            context.clearRect(0, 0, 70, 200)
-            context.drawImage(dino, 0, 40);
-            break;
-        }
-      });
+    window.addEventListener("keypress", (event) => {
+      switch(event.key){
+        case ' ':
+          context.clearRect(0, 0, 70, 200)
+          context.drawImage(dino_img, 0, 40);
+          break;
+      }
+    });
 
     window.requestAnimationFrame(loop);
   }
